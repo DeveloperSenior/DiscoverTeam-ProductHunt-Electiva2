ec2b4256d3f9914d871e800e8f16c10a65551368
DeveloperSenior <andresfescobar92@gmail.com>
2024-03-22 11:50:09 -0500

    ADD back & front project base

---
+++ discover-app-api/routes/config/SuscriptionRoutesAppConf.js
@@ -0,0 +1,9 @@
+/**
+ * Modulo que define los router a publicar en el contenedor
+ */
+const routersApp = [
+    require('./SwaggerRouteConf')
+    /** Import routes here */
+];
+
+module.exports = routersApp
